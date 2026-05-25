98f922089526d8f42e2b8226e3dbc598d5c025b8
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-05-25 23:21:41 +0530

    async added

--- app.test.js
+++ app.test.js
@@ -1,12 +1,12 @@
 const server=require('./app');
 
-describe('Default 200 ok Test', () => {
+describe('Default 200 ok Test', async () => {
     const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
 })
 
 
-describe('Non Existing 400 Not ok Test', () => {
+describe('Non Existing 400 Not ok Test', async () => {
     const response = await request(server).get('/random');
     expect(response.statusCode).toBe(400);
 })
