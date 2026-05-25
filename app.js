aba2586fe7752ceb50acdb912bc99d7a40c8d0f0
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-05-25 17:51:36 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const http=require('http');
+const fs=require('fs');
+const path=require('path');
+
 const port=9000
 
 
 const server=http.createServer((req,res)=>{
     if(req.url==='/'){
+        const file=path.join(__dirname,'./SPA/index.htm');
+        fs.readFile(file,(err,data)=>{
+            if(err){
+                res.writeHead(500,{'Content-Type':'text/plain'});
+                res.end('Internal Server Error');
+                return;
+            }
+            else{
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.end(data);
+            }
+        })
+
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end('./SPA/index.htm');
     }
