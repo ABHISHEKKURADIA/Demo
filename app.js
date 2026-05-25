0d0d33d85bf4b547af7bc1d3131db05df673621f
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-05-25 19:05:45 +0530

    404 added

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const http=require('http');
-const fs=require('fs');
-const path=require('path');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const port=9000
+const port = 9000
 
 
-const server=http.createServer((req,res)=>{
-    if(req.url==='/'){
-        const file=path.join(__dirname,'SPA','index.htm');
-        fs.readFile(file,(err,data)=>{
-            if(err){
-                res.writeHead(500,{'Content-Type':'text/plain'});
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        const file = path.join(__dirname, 'SPA', 'index.htm');
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Internal Server Error');
                 return;
             }
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.end(data);
-                res.end();
-            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
         })
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
     }
 })
 
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
