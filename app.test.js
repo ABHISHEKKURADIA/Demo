22a5f5ae9b04501302fe123bae193e68aa78f594
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-05-25 23:28:15 +0530

    async corrected

--- app.test.js
+++ app.test.js
@@ -1,12 +1,16 @@
-const server=require('./app');
+const server = require('./app');
 
-describe('Default 200 ok Test', async () => {
-    const response = await request(server).get('/');
-    expect(response.statusCode).toBe(200);
+describe('Default 200 ok Test', () => {
+    it('should successfully GET the root path', async () => {
+        const response = await request(server).get('/');
+        expect(response.statusCode).toBe(200);
+    });
 })
 
 
-describe('Non Existing 400 Not ok Test', async () => {
-    const response = await request(server).get('/random');
-    expect(response.statusCode).toBe(400);
+describe('Non Existing 404 Not ok Test', () => {
+    it('shouldnt be successfully GET the root path', async () => { 
+        const response = await request(server).get('/hidden');
+        expect(response.statusCode).toBe(404);
+    });
 })
