06c2b8809e4f9cf2f3085f9af30610ff57964e78
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-05-30 17:48:46 +0530

    EC2 Added in terraform

--- Infrastructure.tf
+++ Infrastructure.tf
@@ -55,6 +55,45 @@ resource "aws_route_table_association" "publicRouteTableAssociation" {
     route_table_id = aws_route_table.publicRouteTable.id
 }
 
+resource "aws_security_group" "customSG" {
+    name = "customSG"
+    vpc_id = aws_vpc.custom_vpc.id
+    ingress {
+        from_port  = 0
+        to_port    = 0
+        protocol   = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
 
 
-
+resource "aws_instance" "web_server" {
+    ami = "ami-07a00cf47dbbc844c"
+    instance_type = "t2.medium"
+    subnet_id = aws_subnet.public_subnet.id
+    security_groups = [aws_security_group.customSG.name]
+    tags = {
+        Name = "web_server"
+    }
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install docker.io -y
+                sudo systemctl start docker
+                sudo systemctl enable docker
+                sudo apt update -y
+                sudo apt update
+                sudo apt install fontconfig openjdk-21-jre
+                java -version
+                sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+                https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+                echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+                https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+                /etc/apt/sources.list.d/jenkins.list > /dev/null
+                sudo apt update
+                sudo apt install jenkins -y
+                sudo systemctl start jenkins
+                sudo systemctl enable jenkins
+            EOF
+}
